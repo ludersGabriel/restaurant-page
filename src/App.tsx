97aad3833b7ce2d15ca17b8bd62e6667bc64dc72
Gabriel Lüders <ludersdev@gmail.com>
2022-01-29 10:14:16 -0300

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import { Navbar } from "./components"
+import { AboutUs, Chef, FindUs, Footer, Gallery, Header, Intro, Laurels, SpecialMenu } from "./container"
+
+const App = () => {
+  return (
+    <main>
+      <Navbar />
+      <Header />
+      <AboutUs />
+      <SpecialMenu />
+      <Chef />
+      <Intro />
+      <Laurels />
+      <Gallery />
+      <FindUs />
+      <Footer />
+    </main>
+  )
+}
+
+export default App
+
